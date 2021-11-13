df8441d98f1b8e435e81b5e88961a46405b1434e
RedEagleV1 <91380930+RedEagleV1@users.noreply.github.com>
2021-11-13 19:06:27 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,343 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const ayarlar = require("./ayarlar.json");
+const chalk = require("chalk");
+const moment = require("moment");
+var Jimp = require("jimp");
+const { Client, Util } = require("discord.js");
+const fs = require("fs");
+const db = require("quick.db");
+const http = require("http");
+const express = require("express");
+require("./util/eventLoader")(client);
+const path = require("path");
+const request = require("request");
+require("discord-buttons")(client);
+
+
+
+
+
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Pinglendi.");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
+var prefix = ayarlar.prefix;
+
+const log = message => {
+  console.log(`${message}`);
+};
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir('./komutlar/', (err, files) => {
+    if (err) console.error(err);
+    log(`${files.length} komut yüklenecek.`);
+    files.forEach(f => {
+        let props = require(`./komutlar/${f}`);
+        log(`Yüklenen komut: ${props.help.name}.`);
+        client.commands.set(props.help.name, props);
+        props.conf.aliases.forEach(alias => {
+            client.aliases.set(alias, props.help.name);
+        });
+    });
+});
+
+
+
+
+client.reload = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            delete require.cache[require.resolve(`./komutlar/${command}`)];
+            let cmd = require(`./komutlar/${command}`);
+            client.commands.delete(command);
+            client.aliases.forEach((cmd, alias) => {
+                if (cmd === command) client.aliases.delete(alias);
+            });
+            client.commands.set(command, cmd);
+            cmd.conf.aliases.forEach(alias => {
+                client.aliases.set(alias, cmd.help.name);
+            });
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+client.load = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            let cmd = require(`./komutlar/${command}`);
+            client.commands.set(command, cmd);
+            cmd.conf.aliases.forEach(alias => {
+                client.aliases.set(alias, cmd.help.name);
+            });
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+
+
+
+client.unload = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            delete require.cache[require.resolve(`./komutlar/${command}`)];
+            let cmd = require(`./komutlar/${command}`);
+            client.commands.delete(command);
+            client.aliases.forEach((cmd, alias) => {
+                if (cmd === command) client.aliases.delete(alias);
+            });
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+client.elevation = message => {
+  if (!message.guild) {
+    return;
+  }
+  let permlvl = 0;
+  if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+  if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+  if (message.author.id === ayarlar.owner) permlvl = 8;
+  return permlvl;
+};
+
+var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
+// client.on('debug', e => {
+//   console.log(chalk.bgBlue.green(e.replace(regToken, 'that was redacted')));
+// });
+
+client.on("warn", e => {
+  console.log(chalk.bgYellow(e.replace(regToken, "that was redacted")));
+});
+
+client.on("error", e => {
+  console.log(chalk.bgRed(e.replace(regToken, "that was redacted")));
+});
+
+client.login(process.env.token);
+//TOKENİ ENVDEN ÇIKARMAYIN VE KİMSEYE PAYLAŞMAYIN ALTYAPIYI REMİXLEDİĞİNİZ ANDA SORUMLULUK SİZE AİTDİR.
+client.on("ready", () => {
+  client.channels.cache.get("905495858743738398").join();   
+})
+
+///////////// KOMUTLAR BAŞ
+
+client.on('voiceStateUpdate', async (oldState, newState) => {
+  if (newState.channel != null && newState.channel.name.startsWith('(2)👉 Tıklayın 👈')) {newState.guild.channels.create(`𝓥İ𝓟 ${newState.member.displayName}`, {type: 'voice',
+    parent: newState.channel.parent,})
+   .then((cloneChannel) => {newState.setChannel(cloneChannel);
+    cloneChannel.setUserLimit(2);})}
+  if (newState.channel != null && newState.channel.name.startsWith('(3)👉 Tıklayın 👈')) {newState.guild.channels.create(`𝓥İ𝓟 ${newState.member.displayName}`, {type: 'voice',
+    parent: newState.channel.parent,})
+   .then((cloneChannel) => {newState.setChannel(cloneChannel);
+    cloneChannel.setUserLimit(3);})}
+if (newState.channel != null && newState.channel.name.startsWith('(4)👉 Tıklayın 👈')) {newState.guild.channels.create(`𝓥İ𝓟 ${newState.member.displayName}`, {type: 'voice',
+    parent: newState.channel.parent,})
+   .then((cloneChannel) => {newState.setChannel(cloneChannel);
+    cloneChannel.setUserLimit(4);})}
+if (newState.channel != null && newState.channel.name.startsWith('(5)👉 Tıklayın 👈')) {newState.guild.channels.create(`𝓥İ𝓟 ${newState.member.displayName}`, {type: 'voice',
+    parent: newState.channel.parent,})
+   .then((cloneChannel) => {newState.setChannel(cloneChannel);
+    cloneChannel.setUserLimit(5);})}
+if (newState.channel != null && newState.channel.name.startsWith('(15)👉 Tıklayın 👈')) {newState.guild.channels.create(`𝓥İ𝓟 ${newState.member.displayName}`, {type: 'voice',
+    parent: newState.channel.parent,})
+   .then((cloneChannel) => {newState.setChannel(cloneChannel);
+    cloneChannel.setUserLimit(15);})}
+// Kullanıcı ses kanalından ayrılınca ve kanalda kimse kalmazsa kanalı siler;
+if (oldState.channel != undefined) {
+  if (oldState.channel.name.startsWith('𝓥İ𝓟')) {
+    if (oldState.channel.members.size == 0) {oldState.channel.delete();}
+      else { // İlk kullanıcı ses kanalından ayrılınca kanaldaki başka kullanıcı adını kanal adı yapar.
+        let matchMember = oldState.channel.members.find(x => `𝓥İ𝓟 ${x.displayName} kanalı` == oldState.channel.name);
+        if (matchMember == null) {
+        oldState.channel.setName(`𝓥İ𝓟 ${oldState.channel.members.random().displayName} kanalı`)
+          }
+       }
+     }
+   }
+});
+
+////////////// KOMUTLAR SON
+////////////// ALTI ELLEME
+
+
+client.on("guildCreate", guild => {
+
+  let murphy = guild.owner
+  
+const dcs = new Discord.RichEmbed()
+.setTitle(`Merhaba!`)
+.setThumbnail(client.user.avatarURL)
+.setTimestamp()
+.setColor("GREEN")//dcs
+.addField('Prefixim', ayarlar.prefix)
+.addField(`Destek Sunucusu`, `Buraya destek Sunucunuzn Linkini Koyun`)
+murphy.send(dcs)
+});
+
+client.on("message", async message => {
+  if(message.author.id === client.user.id) return;
+  if(message.guild) return;
+ //DCS 
+client.channels.get('906206502480670720').send(new Discord.RichEmbed().setAuthor('Yeni DM!').setFooter('DM-LOG SİSTEMİ!').setDescription(`Gönderen kişi:   ${message.author.tag}`).setTimestamp().setThumbnail(client.user.avatarURL).addField("Mesajı;",
+message.content).setColor("GOLD"))//DCS!
+})
+
+client.on("guildMemberAdd", async(member) => {
+  let sunucupaneli = await db.fetch(`sunucupanel_${member.guild.id}`)
+  if(sunucupaneli) {
+    let rekoronline = await db.fetch(`panelrekor_${member.guild.id}`)
+    let toplamuye = member.guild.channels.find(x =>(x.name).startsWith("Toplam Üye •"))
+    let toplamaktif = member.guild.channels.find(x =>(x.name).startsWith("Aktif Üye •"))
+    let botlar = member.guild.channels.find(x =>(x.name).startsWith("Botlar •"))
+    let rekoraktif = member.guild.channels.find(x =>(x.name).startsWith("Rekor Aktiflik •"))
+    
+    if(member.guild.members.filter(off => off.presence.status !== 'offline').size > rekoronline) {
+      db.set(`panelrekor_${member.guild.id}`, member.guild.members.filter(off => off.presence.status !== 'offline').size)
+    }
+    try{
+      toplamuye.setName(`Toplam Üye • ${member.guild.members.size}`)
+      toplamaktif.setName(`Aktif Üye • ${member.guild.members.filter(off => off.presence.status !== 'offline').size}`)
+      botlar.setName(`Botlar • ${member.guild.members.filter(m => m.user.bot).size}`)
+      rekoraktif.setName(`Rekor Aktiflik • ${rekoronline}`)
+   } catch(e) { }
+  }
+})
+
+client.on("guildMemberRemove", async(member) => {
+  let sunucupaneli = await db.fetch(`sunucupanel_${member.guild.id}`)
+  if(sunucupaneli) {
+    let rekoronline = await db.fetch(`panelrekor_${member.guild.id}`)
+    let toplamuye = member.guild.channels.find(x =>(x.name).startsWith("Toplam Üye •"))
+    let toplamaktif = member.guild.channels.find(x =>(x.name).startsWith("Aktif Üye •"))
+    let botlar = member.guild.channels.find(x =>(x.name).startsWith("Botlar •"))
+    let rekoraktif = member.guild.channels.
+    find(x =>(x.name).startsWith("Rekor Aktiflik •"))
+    
+    if(member.guild.members.filter(off => off.presence.status !== 'offline').size > rekoronline) {
+      db.set(`panelrekor_${member.guild.id}`, member.guild.members.filter(off => off.presence.status !== 'offline').size)
+    }
+    try{
+      toplamuye.setName(`Toplam Üye • ${member.guild.members.size}`)
+      toplamaktif.setName(`Aktif Üye • ${member.guild.members.filter(off => off.presence.status !== 'offline').size}`)
+      botlar.setName(`Botlar • ${member.guild.members.filter(m => m.user.bot).size}`)
+      rekoraktif.setName(`Rekor Aktiflik • ${rekoronline}`)
+   } catch(e) { }
+  }
+});
+
+const guildInvites = new Map();
+client.on('inviteCreate', async invite => guildInvites.set(invite.guild.id, await invite.guild.fetchInvites()));
+client.on('ready', () => {
+    client.guilds.cache.forEach(guild => {
+        guild.fetchInvites()
+            .then(invites => guildInvites.set(guild.id, invites))
+            .catch(err => console.log(err));
+    });
+});
+
+client.on('guildMemberAdd', async member => {//hamzamertakbaba#3361
+    const cachedInvites = guildInvites.get(member.guild.id);
+    const newInvites = await member.guild.fetchInvites();
+    guildInvites.set(member.guild.id, newInvites);
+    try {
+        const usedInvite = newInvites.find(inv => cachedInvites.get(inv.code).uses < inv.uses) || "1";
+        const embed = new Discord.MessageEmbed()
+            .setDescription(`${member.user.tag} Sunucuya ${member.guild.memberCount}. sırayla katıldı. ${usedInvite.inviter.tag} tarafından davet edilmiş. ${usedInvite.url} Davet koduyla katılmış. Bu davet kodu ${usedInvite.uses} kere kullanılmış.`)
+            .setTimestamp()
+            .setFooter("31 ZORT CU .")
+        const welcomeChannel = member.guild.channels.cache.find(channel => channel.id === 'LOG KANALI ID');
+        if(welcomeChannel) {
+            welcomeChannel.send(embed).catch(err => console.log(err));
+        }
+    }
+    catch(err) {
+        console.log(err);
+    }
+});
+
+client.on('guildCreate', guild => {
+  const embed = new Discord.RichEmbed()
+  .setColor('RANDOM')
+  .setTitle('Bir Sunucuya Eklendim!')
+  .setDescription(`**${client.user.username}** bot **"${guild.name}"** Adlı Sunucuya Eklendi. Sunucu Üye Sayısı: **${guild.memberCount}** Üye!`)
+  .setFooter(`${client.user.username}`, client.user.avatarURL)
+  .setTimestamp()
+  client.channels.get('906880503045558292').send(embed);
+});
+
+client.on('guildDelete', guild => {
+  const embed = new Discord.RichEmbed()
+  .setColor('RANDOM')
+  .setTitle('Bir Sunucudan Atıldım!')
+  .setDescription(`**${client.user.username}** bot **"${guild.name}"** Adlı Sunucudan Atıldı.`)
+  .setFooter(`${client.user.username}`, client.user.avatarURL)
+  .setTimestamp()
+  client.channels.get('906880503045558292').send(embed);
+});
+client.on("message", async msg => {
+
+  let saas = await db.fetch(`saas_${msg.guild.id}`);
+
+  if (saas == 'kapali') return;
+
+  if (saas == 'acik') {
+
+  if (msg.content.toLowerCase() === 'sa') {
+
+    msg.reply('Aleyküm Selam');
+
+  }
+
+  }
+
+});
+client.on("message", msg => {
+  var dm = client.channels.cache.get("907321674322567218");
+  if (msg.channel.type === "dm") {
+    if (msg.author.id === client.user.id) return;
+    const botdm = new Discord.MessageEmbed()
+      .setTitle(`${client.user.username} Dm`)
+      .setTimestamp()
+      .setColor("RANDOM")
+      .setThumbnail(`${msg.author.avatarURL()}`)
+      .addField("Gönderen", msg.author.tag)
+      .addField("Gönderen ID", msg.author.id)
+      .addField("Gönderilen Mesaj", msg.content);
+
+    dm.send(botdm);
+  }
+  if (msg.channel.bot) return;
+});
+client.on('guildMemberAdd', async member => {
+  if(!db.kontrol(`otorol_${member.guild.id}`)) db.yaz(`otorol_${member.guild.id}`, "deaktif")
+    const gereksiz = await db.fetch(`otorol_${member.guild.id}`);
+    if (gereksiz === "aktif") {
+        var rolke = db.fetch(`otorolu_${member.guild.id}`);
+        var kanal = db.fetch(`otokanal_${member.guild.id}`);
+        member.roles.add(rolke)
+        if(!kanal) { return; } else {
+        client.channels.cache.get(kanal).send(`<@${member.id}> adlı kullanıcıyı başarıyla kaydettim`)
+        }
+    } else if (gereksiz === "deaktif") {
+    }
+    if (!gereksiz) return;
+});
+
+
+ 
